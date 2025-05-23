30134d75dc0da2efebf9d2483493bc548bf32d68
kevinkim573 <kimkevin573@gmail.com>
2025-05-23 20:19:55 +0300

    modify test cov settings

--- jest.config.js
+++ jest.config.js
@@ -9,12 +9,12 @@ const config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
+    // global: {
+    //   branches: 80,
+    //   functions: 80,
+    //   lines: 80,
+    //   statements: 80,
+    // },
   },
 }
 
